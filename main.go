23467dc56b8d75baa5adfd0af22704e4baea8d45
Lucas Tavares <lucastavaresdarocha@gmail.com>
2017-08-15 19:28:43 -0300

    menu

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	nome := "Lucas"
+	const idade = 20
+	const versao float32 = 1.1
+
+	fmt.Println("Hello,", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair")
+
+	var comando int
+
+	fmt.Scan(&comando)
+
+	fmt.Println("O comando escolhido foi", comando)
 }
